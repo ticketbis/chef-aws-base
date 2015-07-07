eebc6b6f59e621fab9f56887ac706dc674a37d65
Alberto Tablado <alberto@aluki.org>
2015-07-07 18:31:07 +0200

    Repository address

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 name 'aws-base'
 maintainer 'Alberto Tablado'
 maintainer_email 'alberto.tablado@ticketbis.com'
+source_url 'https://github.com/ticketbis/chef-aws-base'
 license 'GPL v3'
 description 'Base recipe for working with Amazon AWS'
 long_description IO.read(File.join(
