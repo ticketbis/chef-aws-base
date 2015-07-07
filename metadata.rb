19223d6e1e726d53d48ad2a2a828608cd624bc4e
Alberto Tablado <alberto@aluki.org>
2015-07-08 00:14:30 +0200

    License change

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'aws-base'
 maintainer 'Alberto Tablado'
 maintainer_email 'alberto.tablado@ticketbis.com'
 source_url 'https://github.com/ticketbis/chef-aws-base'
-license 'GPL v3'
+license 'Apache v2.0'
 description 'Base recipe for working with Amazon AWS'
 long_description IO.read(File.join(
   File.dirname(__FILE__), 'README.md'
